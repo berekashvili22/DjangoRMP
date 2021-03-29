b3e97465927c1e4a7d84a4a9ef49df6790ba1ef6
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-29 22:26:45 +0400

    create custom user model

---
+++ RateMyProfessors/Account/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from . models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea
+
+
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    search_fields = ('email', 'username',)
+    list_filter = ('email', 'username', 'is_active', 'is_staff')
+    ordering = ('-date_joined',)
+    list_display = ('email', 'username',
+                    'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'username',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(CustomUser, UserAdminConfig)
