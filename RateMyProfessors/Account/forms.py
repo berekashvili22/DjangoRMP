47d7511e96ed4f616aa126c5372c4b6ce910741b
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-04-15 23:14:48 +0400

    change abstruser model name to user

--- RateMyProfessors/Account/forms.py
+++ RateMyProfessors/Account/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from . models import CustomUser
+from . models import Account
 from django.contrib.auth.forms import UserCreationForm
 
 class CreateUserForm(UserCreationForm):
     class Meta:
-        model = CustomUser
+        model = Account
         fields = ['username', 'email', 'password1', 'password2']
