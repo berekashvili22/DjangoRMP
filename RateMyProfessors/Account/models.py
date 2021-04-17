042ac8b3ba6a862b03635230908cf15cee770a0d
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-04-17 22:59:35 +0400

    create profile model

--- RateMyProfessors/Account/models.py
+++ RateMyProfessors/Account/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
 from django.db import models
@@ -52,3 +53,27 @@ class Account(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Profile(models.Model):
+    account = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE,
+        # primary_key=True,
+        null=True,
+    )
+    image = models.ImageField(
+        null=True, default='default.png', upload_to='profile_pics')
+    
+    def __str__(self):
+        return f'{self.account} Profile'
+    
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
