e1e674ecde5afeb21c0714b3b576ac1c870264eb
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-04-17 23:00:12 +0400

    create forms for profile update

--- RateMyProfessors/Account/forms.py
+++ RateMyProfessors/Account/forms.py
@@ -1,8 +1,21 @@
 from django import forms
-from . models import Account
-from django.contrib.auth.forms import UserCreationForm
+from . models import Account, Profile
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = Account
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(UserChangeForm):
+    class Meta:
+        model = Account
+        fields = ['username', 'email']
+
+
+class ImageUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
