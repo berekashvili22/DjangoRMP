75da89bdcc570c302e5f824437b0222f280823ce
tornike <tokoberekashvili22@gmail.com>
2021-05-09 20:45:20 +0400

    commit reverse

--- RateMyProfessors/pages/static/pages/js/navbar.js
+++ RateMyProfessors/pages/static/pages/js/navbar.js
@@ -1,11 +1,13 @@
 const navSlide = ()=> {
-         const burger = document.querySelector('.navbar__burger');
-         const nav = document.querySelector('.navbar');
-         var body = document.getElementsByTagName('body')[0];
+         const burger = document.querySelector('.nav__burger');
+         const menu = document.querySelector('.menu');
+        //  var body = document.getElementsByTagName('body')[0];
      
          burger.addEventListener('click', ()=>{
-             body.classList.toggle('hide-flow');
-             nav.classList.toggle('responsive')
+             console.log('clicked')
+             menu.classList.toggle('menu-resp-active')
+            //  body.classList.toggle('hide-flow');
+            //  nav.classList.toggle('responsive')
              burger.classList.toggle('toggleBurg');
      
      
